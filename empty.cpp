d3821bb940f4005c7aa6e10ac38bfd06aa39e5be
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-05-08 12:24:04 +0600

    check a vector empty or not .

---
+++ empty.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+  vector<int> v{1,3,4};
+  //cout << v.empty() << endl;// if vector is empty then print 1 else
+  0;
+  cout << v.empty() << endl;
+  return 0;
+}
